6ca9062b637786ab8ca0b4dc3eab6460ef7edc82
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-08 00:46:39 +0530

    actions

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -2,5 +2,6 @@ module "sg" {
     source              = "../../terraform-aws-sg-module" # In projects, we use "git::https://github.com/javeed-mohd/terraform-aws-vpc.git?ref=main", to directly get it from git if someone updated...
     project             = var.project # project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
     environment         = var.environment
-    sg_name             =
+    sg_name             = "mongodb"
+    vpc_id              = local.vpc_id
 }
