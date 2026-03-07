46154e33206fbb6fc0ac461bc3ca1409d6d69157
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-08 00:13:05 +0530

    actions

--- 00-vpc/variables.tf
+++ 00-vpc/variables.tf
@@ -1,4 +1,5 @@
 # project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
+# First only main, variables and provider
 variable "project" {
   default   = "roboshop"
 }
