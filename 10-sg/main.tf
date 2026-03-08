f213aa3957564d26da2241dd3376a1abe4b23b9f
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-08 15:27:10 +0530

    actions

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,7 +1,8 @@
 module "sg" {
-    source              = "../../terraform-aws-sg-module" # In projects, we use "git::https://github.com/javeed-mohd/terraform-aws-vpc.git?ref=main", to directly get it from git if someone updated...
-    project             = var.project # project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
-    environment         = var.environment
-    sg_name             = "mongodb"
-    vpc_id              = local.vpc_id
+    count       = length(var.sg_names) # Gives the count of security groups.
+    source      = "../../terraform-aws-sg-module" # In projects, we use "git::https://github.com/javeed-mohd/terraform-aws-vpc.git?ref=main", to directly get it from git if someone updated...
+    project     = var.project # project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
+    environment = var.environment
+    sg_name     = "mongodb"
+    vpc_id      = local.vpc_id
 }
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -7,9 +7,20 @@ variable "environment" {
   default   = "dev"
 }
 
-/* variable "sg_names" {
+variable "sg_names" {
   type      = list
   default   = [
-
+    # Databases
+    "mongodb", "redis", "mysql", "rabbitmq",
+    # Backend
+    "catalogue", "user", "cart", "shipping", "payment",
+    # Backend Application Load Balancer(ALB)
+    "backend_alb",
+    # Frontend
+    "frontend",
+    # Frontend Application Load Balancer(ALB)
+    "frontend_alb",
+    # Bastion --> A bastion host is a special server that acts as a secure gateway to access resources inside a private network (like private VMs or instances).
+    "bastion" 
   ]
-} */
+}
