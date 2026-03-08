c1d0c2ee08f4ac6a349c41972002cadde67fd00a
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-08 15:05:54 +0530

    actions

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,22 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+# We get list of ids as output, so we use Stringlist while creating ids of subnets...
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids" # /roboshop/dev/public_subnet_ids
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/private_subnet_ids" # /roboshop/dev/private_subnet_ids
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/database_subnet_ids" # /roboshop/dev/database_subnet_ids
+  type  = "StringList"
+  value = join(",", module.vpc.database_subnet_ids)
+}
