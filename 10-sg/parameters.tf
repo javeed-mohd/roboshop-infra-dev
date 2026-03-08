70e5a5a78ea8be918a8df6b6cfa3f68f1171ea9d
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-08 15:47:24 +0530

    actions

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -9,5 +9,5 @@ resource "aws_ssm_parameter" "sg_id" {
   count = length(var.sg_names)
   name  = "/${var.project}/${var.environment}/${var.sg_names[count.index]}_sg_id" # /roboshop/dev/(component)_sg_id
   type  = "String"
-  value = module.sg.sg_id
+  value = module.sg[count.index].sg_id
 }
