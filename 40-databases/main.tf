9a4a1487f6322ee0829a4e8f90168789e71268b7
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-09 20:12:22 +0530

    actions

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "mongodb" {
   ami                       = local.ami_id
   instance_type             = "t3.micro"
-  subnet_id                 = local.public_subnet_id
-  vpc_security_group_ids    = [local.bastion_sg_id]    # List type
+  subnet_id                 = local.database_subnet_id
+  vpc_security_group_ids    = [local.mongodb_sg_id]    # List type
 
-  # roboshop-dev-bastion
+  # roboshop-dev-mongodb
   tags = merge(
     {
-        Name = "${var.project}-${var.environment}-bastion"
+        Name = "${var.project}-${var.environment}-mongodb"
     },
     local.common_tags
   )
