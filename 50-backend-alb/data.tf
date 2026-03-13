ad96ff657a1a6ee28deb318059af8838a23078a9
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-13 22:28:12 +0530

    actions

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -1,10 +1,10 @@
 # data-source
-# For Private Subnet Id ssm parameter store creation
-data "aws_ssm_parameter" "private_subnet_ids" {
-    name = "/${var.project}/${var.environment}/private_subnet_ids"     # /roboshop/dev/private_subnet_ids
-}
-
 # For Backend ALB sg id ssm parameter store creation
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id" # /roboshop/dev/backend_alb_sg_id
+}
+
+# For Private Subnet Id ssm parameter store creation
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"     # /roboshop/dev/private_subnet_ids
 }
--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,12 +1,13 @@
+# Backend ALB creation
 resource "aws_lb" "backend_alb" {
   name               = "${var.project}-${var.environment}" # roboshop-dev
-  internal           = false
+  internal           = true # Because it is private
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
 
-  # keeping it as false, just to delete using terraform while practice
-  enable_deletion_protection = true
+  # Keeping it as false, just to delete using terraform while practice
+  enable_deletion_protection = false
 
   tags = merge(
     {
