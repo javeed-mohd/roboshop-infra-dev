5b6d49567163cb22bf24864de3ffd2da8a7c42a6
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-13 21:39:34 +0530

    actions

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -4,7 +4,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"     # /roboshop/dev/private_subnet_ids
 }
 
-# For Backend ALB ssm parameter store creation
+# For Backend ALB sg id ssm parameter store creation
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id" # /roboshop/dev/backend_alb_sg_id
 }
