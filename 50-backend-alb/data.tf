e654701b032f598f4a22e7b30ac77c0d702b8436
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-14 16:47:24 +0530

    actions

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -1,10 +1,10 @@
-# data-source
-# For Backend ALB sg id ssm parameter store creation
+# Data sources are used to query and fetch existing information from your provider like AWS, so you can use that data in your Terraform configuration.
+# For Backend ALB sg id ssm parameter store
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id" # /roboshop/dev/backend_alb_sg_id
 }
 
-# For Private Subnet Id ssm parameter store creation
+# For Private Subnet Id ssm parameter store
 data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"     # /roboshop/dev/private_subnet_ids
 }
