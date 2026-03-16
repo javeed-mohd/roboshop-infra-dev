330afb21992edff23cc467b62151d87c68393219
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-16 17:58:59 +0530

    actions

---
+++ 80-frontend-alb/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    # Public Subnet in us-east-1a  Availability Zone
+    frontend_alb_sg_id   = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    public_subnet_ids  = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+    common_tags = {
+        Project     = var.project
+        Environment = var.environment
+        Terraform   = "true"
+    }
+}
---
+++ 80-frontend-alb/parameters.tf
@@ -0,0 +1,6 @@
+# For Listener Rule (Backend ALB) creation in 60-catalogue folder
+resource "aws_ssm_parameter" "backend_alb_listener_arn" {
+  name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"     # /roboshop/dev/backend_alb_listener_arn
+  type  = "String"
+  value = aws_lb_listener.http.arn
+}
