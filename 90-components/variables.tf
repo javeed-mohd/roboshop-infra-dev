a234caea4268f9457bc23b698d2f64557f3df74f
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-18 00:16:23 +0530

    actions

---
+++ 90-components/variables.tf
@@ -0,0 +1,24 @@
+variable "components" {
+    default = {
+        # Backend components (Catalogue, User, Cart, Shippping & Payment) are attaching to Backend ALB
+        catalogue = {
+            rule_priority = 10
+        }
+        user = {
+            rule_priority = 20
+        }
+        cart = {
+            rule_priority = 30
+        }
+        shipping = {
+            rule_priority = 40
+        }
+        payment = {
+            rule_priority = 50
+        }
+        # Frontend component (Frontend) is only component attaching to Frontend ALB
+        frontend = {
+            rule_priority = 10
+        }
+    }
+}
