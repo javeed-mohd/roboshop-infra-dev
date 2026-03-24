79537e4a2878d806d2b38acb5f4404fb2d31ad50
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-24 19:38:31 +0530

    actions

--- 98-openvpn/data.tf
+++ 98-openvpn/data.tf
@@ -1,12 +1,12 @@
 # Data sources are used to query and fetch existing information from your provider like AWS, so you can use that data in your Terraform configuration.
-# AMI-ID data source
+# AMI-ID Openvpn data source
 data "aws_ami" "openvpn" {
   most_recent      = true
-  owners           = ["973714476881"]
+  owners           = ["679593333241"]
 
   filter {
     name   = "name"
-    values = ["Redhat-9-DevOps-Practice"]
+    values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
   }
 
   filter {
@@ -18,4 +18,14 @@ data "aws_ami" "openvpn" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+}
+
+# Public Subnet Id data source
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"     # /roboshop/dev/public_subnet_ids
+}
+
+# Openvpn SG Id data source
+data "aws_ssm_parameter" "openvpn_sg_id" {
+    name = "/${var.project}/${var.environment}/openvpn_sg_id"     # /roboshop/dev/openvpn_sg_id
 }
---
+++ 98-openvpn/main.tf
@@ -0,0 +1,16 @@
+# Creation of Openvpn EC2 Instance
+resource "aws_instance" "openvpn" {
+  ami                       = local.ami_id
+  instance_type             = "t3.small"
+  subnet_id                 = local.public_subnet_id
+  vpc_security_group_ids    = [local.openvpn_sg_id]
+  user_data                 = file("vpn.sh")
+
+  # roboshop-dev-openvpn
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-openvpn"
+    },
+    local.common_tags
+  )
+}
