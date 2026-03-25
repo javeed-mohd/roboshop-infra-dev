2a390fee1627d1f22ab24ede2aa21cdf1ae09262
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-25 19:39:22 +0530

    actions

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -10,3 +10,20 @@ xfs_growfs /home
 yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
+
+# For Practicing purpose
+# Creating Databases (MongoDB (27017), Redis (6379), MySQL (3306), RabbitMQ (5672))
+cd /home/ec2-user
+git clone https://github.com/javeed-mohd/roboshop-infra-dev.git
+chown ec2-user:ec2-user -R roboshop-infra-dev
+cd roboshop-infra-dev/40-databases
+terraform init
+terraform apply -auto-approve
+
+# Creating Components (Catalogue (8080), User (8080), Cart (8080), Shipping (8080), Payment (8080), Frontend (80))
+cd /home/ec2-user
+git clone https://github.com/javeed-mohd/roboshop-infra-dev.git
+chown ec2-user:ec2-user -R roboshop-infra-dev
+cd roboshop-infra-dev/90-components
+terraform init
+terraform apply -auto-approve
