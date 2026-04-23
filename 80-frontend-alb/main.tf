1b356e2934beabca7166c1fc1d1593adac4eed68
Javeed Mohammed <javeedmohd214@gmail.com>
2026-04-23 18:10:55 +0530

    k8

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -37,6 +37,43 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+resource "aws_lb_target_group" "frontend" {
+  name                  = "${var.project}-${var.environment}-frontend"
+  port                  = 80
+  protocol              = "HTTP"
+  vpc_id                = local.vpc_id
+  deregistration_delay  = 60
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 10
+    matcher             = "200-299"
+    path                = "/"
+    port                = 80
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 3
+  }
+}
+
+# This depends on Target group
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.project}-${var.environment}.${var.domain_name}"]     # roboshop-dev.devopsdaws.online
+    }
+  }
+}
+
+
 # Route53 record
 resource "aws_route53_record" "www" {
   zone_id   = var.zone_id
